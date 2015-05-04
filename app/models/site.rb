6010800189a49d3a923459b87ea337769e2be210
jiikko <n905i.1214@gmail.com>
2015-05-04 17:52:03 +0900

    増えるレコード

--- app/models/site.rb
+++ app/models/site.rb
@@ -4,7 +4,7 @@ class Site < ActiveRecord::Base
   has_one :captured_version, ->{ order(name: :desc).limit(1) }
 
   def create_captured_version_with_inc!
-    last_captured_version = captured_versions.last || captured_versions.create
-    captured_versions.create(name: last_captured_version.name + 1)
+    name = captured_versions.last.try!(:name) || 0
+    captured_versions.create(name: name + 1)
   end
 end
