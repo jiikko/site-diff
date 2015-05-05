b6b16da901059a01396a39e4014754fbbe44192c
jiikko <n905i.1214@gmail.com>
2015-05-05 19:59:04 +0900

    画面から投稿できるようにする

--- app/models/site.rb
+++ app/models/site.rb
@@ -3,6 +3,8 @@ class Site < ActiveRecord::Base
   has_many :captured_versions
   has_one :captured_version, ->{ order(name: :desc).limit(1) }
 
+  accepts_nested_attributes_for :target_pages, allow_destroy: true
+
   def create_captured_version_with_inc!
     name = captured_versions.last.try!(:name) || 0
     captured_versions.create(name: name + 1)
