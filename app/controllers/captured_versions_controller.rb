2fba1eb3a6b64f05f98fed325e9b97304c196ea7
jiikko <n905i.1214@gmail.com>
2015-05-09 17:36:18 +0900

    表示

--- app/controllers/captured_versions_controller.rb
+++ app/controllers/captured_versions_controller.rb
@@ -2,9 +2,11 @@ class CapturedVersionsController < ApplicationController
   def show
     @site = Site.find(params[:site_id])
     @captured_version = CapturedVersion.find(params[:id])
-    @captured_environment_with_names = CapturedEnvironment.
-      includes(captured_page: :target_page).
-      joins(captured_page: :captured_version).
-      where(captured_versions: { id: @captured_version }).group_by { |x| x.name }
+    # @captured_environment_with_names = CapturedEnvironment.
+    #   includes(captured_page: :target_page).
+    #   joins(captured_page: :captured_version).
+    #   where(captured_versions: { id: @captured_version }).group_by { |x| x.name }
+
+    @captured_environments = @captured_version.captured_environments
   end
 end
